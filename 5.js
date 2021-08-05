d45eccf5075dc5e3348309507800f870c03365b6
Suraj Prakash Sahoo <raja.suraj@gmail.com>
2021-08-05 12:11:12 +0530

    final commit: Q5

---
+++ 5.js
@@ -0,0 +1,9 @@
+//Question 5. What will be the output of following code?
+
+var arr = [2, 56, 78, 34, 65];
+var new_arr = arr.map(function(num) {
+return num / 2;
+});
+print(new_arr);
+
+//output: no output but print preview is shown
