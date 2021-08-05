e8ecb8569c588e8ba04135964ed6db98a71c05e9
Suraj Prakash Sahoo <raja.suraj@gmail.com>
2021-08-05 12:01:51 +0530

    final commit: Q3

---
+++ 3.js
@@ -0,0 +1,9 @@
+//Question 3. What will be the output of the following code?
+var Employee =
+{
+company: 'Rohit'
+}
+var Emp1 = Object.create(employee);
+delete Emp1.company Console.log(emp1.company);
+
+//Output: Syntax Error due to missing ';' and ReferenceError due to 'Console' not defined
